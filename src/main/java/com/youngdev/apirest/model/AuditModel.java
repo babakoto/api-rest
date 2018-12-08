efbd55947c8c273f80f8ac78a1216c510b81992e
babakoto <e.tokiniaina@gmail.com>
2018-12-08 14:25:43 +0100

    add abstract class Audit

--- src/main/java/com/youngdev/apirest/model/AuditModel.java
+++ src/main/java/com/youngdev/apirest/model/AuditModel.java
@@ -1,15 +1,31 @@
 package com.youngdev.apirest.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.io.Serializable;
 import java.util.Date;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 public abstract class AuditModel implements Serializable {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false,updatable = false)
+    @CreatedDate
     private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_at",nullable = false)
+    @LastModifiedDate
     private Date updatedAt;
 }
