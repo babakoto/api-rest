7476af9a3ea4d2f7ffd5d53467793829f3f15634
babakoto <e.tokiniaina@gmail.com>
2018-12-08 15:05:06 +0100

    Created Repository Post & Comment

---
+++ src/main/java/com/youngdev/apirest/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.youngdev.apirest.repository;
+
+import com.youngdev.apirest.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
